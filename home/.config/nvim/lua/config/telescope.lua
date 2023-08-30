7602caae2977ab5989663a348b94fd9e27e894c7
jruz <javi@jruz.io>
2023-08-30 14:41:05 +0200

    telescope

--- home/.config/nvim/lua/config/telescope.lua
+++ home/.config/nvim/lua/config/telescope.lua
@@ -1,9 +1,29 @@
 local telescope = require('telescope.builtin')
-local tabs = require('telescope-tabs')
+local tabs      = require('telescope-tabs')
 
-vim.api.nvim_set_keymap("n", "<C-P>",
-  ":Telescope find_files find_command=rg,--no-ignore-vcs,--no-ignore-dot,--hidden,-g,!.git/*,-g,!node_modules/*,-g,!target/*,-g,!.clj-kondo,--files<Enter>",
-  { noremap = true, silent = true })
+local command   = {
+  ":Telescope find_files find_command=rg",
+  "--no-ignore-vcs",
+  "--no-ignore-dot",
+  "--hidden",
+  "-g",
+  "!.git/*",
+  "-g",
+  "!node_modules/*",
+  "-g",
+  "!*/node_modules/*",
+  "-g",
+  "!target/*",
+  "-g",
+  "!.clj-kondo",
+  "-g",
+  "!.cpcache",
+  "-g",
+  "!.lsp",
+  "--files<Enter>"
+}
+
+vim.api.nvim_set_keymap("n", "<C-P>", table.concat(command, ","), { noremap = true, silent = true })
 
 vim.keymap.set('n', '<C-B>', telescope.buffers, {})
 vim.keymap.set('n', '<C-F>', telescope.live_grep, {})
