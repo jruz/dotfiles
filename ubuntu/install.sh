4b57a931aea86c7ebeaf253009760f9f8de11af1
jruz <javi@jruz.io>
2024-02-18 12:45:54 +0100

    WIP

---
+++ ubuntu/install.sh
@@ -0,0 +1,19 @@
+pip install -r requirements.txt
+
+# TODO: install this stuff with ansible
+# flatpak
+# gnome-tweaks \
+#
+# lspci | grep -i nvidia
+# sudo lshw -c video
+# echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nvidia-nouveau.conf
+# sudo update-initramfs -u
+# sudo apt remove nvidia-prime
+# mise install
+# dpkg -i motivewave_6.8.8_amd64.deb
+# nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
+# nix-channel --update
+# systemctl start NetworkManager.service
+# apt install nvidia-prime
+# apt search ^nvidia-driver
+# apt install nvidia-driver-470
